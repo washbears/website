3590a048e019cac03d2318ec027f30b538c16472
Linus Alpsten <linus.alpsten@telia.com>
2016-10-13 11:56:41 +0200

    added home controller and index view

---
+++ washbears website/washbears website/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace washbears_website.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
